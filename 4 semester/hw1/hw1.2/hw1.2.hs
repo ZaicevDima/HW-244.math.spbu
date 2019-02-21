1c19b10760a488f85d449b8e0bc0f3c7b97867a9
ZaicevDima <zaicevdmitiy@gmail.com>
2019-02-21 18:23:53 +0300

    Change fibonacciIterative and fibonacciRecursive using where and

--- 4 semester/hw1/hw1.2/hw1.2.hs
+++ 4 semester/hw1/hw1.2/hw1.2.hs
@@ -1,15 +1,17 @@
 module Fibonacci where
 
-fibonacciSearch :: Integer -> Integer -> Integer -> Integer
-fibonacciSearch previous current 0 = current
-fibonacciSearch previous current 1 = current
-fibonacciSearch previous current n = fibonacciSearch current (current + previous) (n - 1)
-
 fibonacciIterative :: Integer -> Integer
-fibonacciIterative n = if n >= 0 then fibonacciSearch 1 1 n else error "argument must be >= 0"
+fibonacciIterative n 
+	| n < 0 = error "argument must be >= 0"
+	| otherwise = search 1 1 n 
+  	where
+  		search previous current n 
+			| (n == 0) || (n == 1) = current 
+			| otherwise = search current (current + previous) (n - 1)
 
 fibonacciRecursive :: Integer -> Integer
-fibonacciRecursive 0 = 1
-fibonacciRecursive 1 = 1
-fibonacciRecursive n = if n >= 0 then fibonacciRecursive (n - 1) + fibonacciRecursive (n - 2) else error "argument must >= 0"	
+fibonacciRecursive n 
+	| n < 0 = error "argument must be >= 0"
+	| (n == 0) || (n == 1) = 1 
+	| otherwise = fibonacciRecursive (n - 1) + fibonacciRecursive (n - 2) 
 
