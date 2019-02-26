21964bd9e2a19e5b3f139bb133c74856a81a014c
ZaicevDima <zaicevdmitiy@gmail.com>
2019-02-26 23:43:45 +0300

    Fix spaces

--- 4 semester/hw2/hw2.4/hw2.4.hs
+++ 4 semester/hw2/hw2.4/hw2.4.hs
@@ -2,7 +2,7 @@ module SearchFirstEntry where
 
 searchFistEntry :: [Int] -> Int -> Int
 searchFistEntry list value = searchFistEntry' list value 0 where
-  searchFistEntry' (x:xs) value counter 
-    | x == value = counter
-    | length (x:xs) == 1 = error "not found"
-    | otherwise =  searchFistEntry' xs value (counter + 1)
+    searchFistEntry' (x:xs) value counter 
+        | x == value = counter
+        | length (x:xs) == 1 = error "not found"
+        | otherwise =  searchFistEntry' xs value (counter + 1)
